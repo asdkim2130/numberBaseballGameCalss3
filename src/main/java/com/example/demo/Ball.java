d082196fbb50b0663062f526956860cf78722c82
HYOJIN <asdkim2130@gmail.com>
2024-12-09 17:02:58 +0900

    ClassxList, Stack & Queue

--- src/main/java/com/example/demo/Ball.java
+++ src/main/java/com/example/demo/Ball.java
@@ -4,9 +4,12 @@ import java.util.Objects;
 
 public class Ball {
 
-    int position; //자리 저장
-    int number; //값 저장
+    private int position; //자리 저장
+    private int number; //값 저장
 
+
+
+    //Override
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -20,8 +23,59 @@ public class Ball {
         return Objects.hash(position, number);
     }
 
+
+    //생성자
     public Ball(int position, int number) {
         this.position = position;
         this.number = number;
     }
+
+
+    //generate -> Getter
+    public int getPosition() {
+        return position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    void ball (){
+        System.out.println(getPosition());  //출력
+        System.out.println(getNumber());
+    }
+
+
+    //generate -> Setter
+    public void setPosition(int position) {
+        if(position > 3 || position < 1){
+            throw new IllegalArgumentException("Error!");
+        }
+
+        this.position = position;
+    }
+
+
+
+    //ball 비교 함수
+    public BallStatus matchStatus(Ball other) { //Ball이 데이터타입, otherBall이 변수 이름
+        if (this.equals(other)
+//                this.number == other.number && this.position == other.position)
+        )
+        {  //변수이름.number, 변수이름.position
+            return BallStatus.STRIKE;
+        } else if (this.number == other.number) {
+            return BallStatus.BALL;
+
+        }return BallStatus.NOTHING;
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/test/java/com/example/demo/BallTest.java
+++ src/test/java/com/example/demo/BallTest.java
@@ -21,5 +21,18 @@ public class BallTest {
         // 비교 결과는 true가 나와야 한다
         assertThat(result).isEqualTo(true);
 //        assertThat(ball1.equals(ball2)).isEqualTo(true);
+
+
     }
+
+    @Test
+    void matchStatus (){
+
+        Ball computerBall = new Ball(1, 2);
+        Ball userBall = new Ball(1, 2);
+
+        assertThat(computerBall.matchStatus(userBall)).isEqualTo(BallStatus.STRIKE);
+    }
+
+
 }
